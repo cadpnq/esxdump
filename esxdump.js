5b8978ea61d954ad22100644c5d87d295c5a65b7
Nicky Nickell <pnqcad@gmail.com>
2017-08-30 10:43:13 -0400

    handle compressed records, faster streaming reader

--- esxdump.js
+++ esxdump.js
@@ -1,9 +1,10 @@
 const Parser = require("binary-parser").Parser;
-const hasha = require("hasha");
 const sprintf = require("sprintf-js").sprintf;
+const hasha = require("hasha");
+const zlib = require('zlib');
 const fs = require("fs");
 
-let record = new Parser()
+let header = new Parser()
   .endianess("little")
   .string("type", {length: 4})
   .uint32("dataSize")
@@ -12,22 +13,79 @@ let record = new Parser()
   .uint32("revision")
   .uint16("version")
   .uint16("unknown")
-  .buffer("data", {
-    length: "dataSize"
-  });
 
-fs.readFile(process.argv[2], (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
+let field = new Parser()
+  .endianess("little")
+  .string("type", {length: 4})
+  .uint16("size")
+  .buffer("data", {
+    length: "size"
+  })
+
+  let fields = new Parser()
+    .array("fields", {
+      type: field,
+      readUntil: "eof"
+    })
+
+  let compressed = new Parser()
+    .uint32("decompSize")
+    .buffer("compData", {
+      readUntil: "eof"
+    })
+
+let compressed_records = 0;
+let records = 0;
+let groups = 0;
+
+
+let buff;
+let s = fs.createReadStream(process.argv[2]);
+s.on("data", (chunk) => {
+  if (!buff) {
+    buff = chunk;
+  } else {
+    buff = Buffer.concat([buff, chunk]);
   }
-  while (data != "") {
-    let r = record.parse(data);
-    if (r.type == "GRUP") {
-      data = data.slice(24);
-    } else {
-      console.log(`${sprintf("%08x", r.id)} - ${r.type} - ${hasha(r.data, {algorithm: 'md5'})}`);
-      data = data.slice(r.dataSize + 24);
+
+  let h;
+  let data;
+  let fs;
+  while (true) {
+    if (buff.length < 24) {
+      return;
     }
+    h = header.parse(buff);
+
+    if (h.type == "GRUP") {
+      buff = buff.slice(24)
+      groups += 1
+      continue;
+    }
+
+    if (buff.length < h.dataSize+24) {
+      return;
+    }
+
+    data = buff.slice(24, h.dataSize + 24);
+
+    if (h.flags & 0x00040000) {
+      compressed_records += 1;
+      let decomp = compressed.parse(data);
+      data = zlib.inflateSync(decomp.compData);
+    }
+
+    fs = fields.parse(data);
+
+    records += 1;
+    console.log(`${sprintf("%08x", h.id)} - ${h.type}`);
+    for (let f of fs.fields) {
+      console.log(`\t${f.type} - ${hasha(f.data, {algorithm: 'md5'})}`);
+    }
+    buff = buff.slice(h.dataSize + 24);
   }
+});
+
+s.on("end", () => {
+  console.log(`groups: ${groups}\nrecords: ${records}\ntotal: ${groups+records}\ncompressed: ${compressed_records}`)
 });
