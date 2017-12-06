8beaa06dd210d5001791f2e5b63db9399d09d13e
tim-timman <tim.den.vilde@gmail.com>
2017-12-06 06:34:53 +0100

    regexp improvement; common record handling

--- generate.js
+++ generate.js
@@ -4,12 +4,17 @@ const fs = require("fs");
 const boilerplate_requires = `const Parser = require("binary-parser").Parser;
 const sprintf = require("sprintf-js").sprintf;
 const hasha = require("hasha");
-const generic = require("./generic");\n`;
+const generic = require("./generic");`;
 
-const boilerplate_printer = (name) => `// START <${name}> - do not edit; auto-generated comments\n\
-exports['${name}'] = undefined; // undefined until implemented\n// END <${name}>\n\n`;
+const boilerplate_printer = (name, printer = undefined) => `// START <${name}> - do not edit; auto-generated comments\n\
+exports['${name}'] = ${printer};\n// END <${name}>`;
+
+const boilerplate_removed = (code) =>
+`/*********************** AUTO-REMOVED ***********************\n|\n| ${code.replace(/\n/gm,"\n| ")}\
+\n|\n\\${new Date(Date.now()).toLocaleString().padStart(57, "-")} */`;
 
 const fields_file_path = "fields.txt";
+const common_file_path = "common.json"; // use json for now
 const index_file_path = "printers/index.js";
 
 let mappings = new Map();
@@ -24,6 +29,14 @@ if (fs.existsSync(fields_file_path)) {
     return;
 }
 
+let common_data = null;
+if (fs.existsSync(common_file_path)) {
+    try {
+	common_data = JSON.parse(fs.readFileSync(common_file_path, "utf8"));
+    } catch (e) { }
+}
+
+let cur_regexp;
 let cur_record;
 let cur_file_path;
 let cur_file_data;
@@ -31,7 +44,6 @@ let cur_file_data;
 for (let f of new Map([...mappings.entries()].sort())) { // sorting to process one record at a time
     let [record, field] = f[0].split(".");
     let name = `${record}.${field}`;
-    let regexp = new RegExp(String.raw`^\/\/ START <${record}\.${field}>([\s\S])*?exports\['${record}\.${field}'\]([\s\S])*?^\/\/ END <${record}\.${field}>$`, 'm');
 
     if (record !== cur_record) {
 	if (typeof cur_record !== "undefined") {
@@ -40,40 +52,68 @@ for (let f of new Map([...mappings.entries()].sort())) { // sorting to process o
 	cur_record = record;
 	cur_file_path = `printers/${record}.js`;
 	cur_file_data = fs.existsSync(cur_file_path) ? fs.readFileSync(cur_file_path, "utf8") : boilerplate_requires;
+	// RegExp will match all boilerplate printer code, capturing field and export function as capture group 1 and 2
+	cur_regexp = new RegExp(
+	    // NOTE: Node bug, \1 is treated as octal (and needs escape) even when in String.raw``
+	    String.raw`^\/\/ START <${record}\.(\S{4,}?)>(?:[\s\S])*?exports\['${record}\.${"\\1"}'\]\s*?=\s*?((?:[\w\.]|\[[\s\S]*?\])+)(?:[\s\S])*?^\/\/ END <${record}\.${"\\1"}>$`,
+	    'gm'
+	);
     }
-    
-    let regexp_match = regexp.exec(cur_file_data);
-    if (regexp_match === null) {
-	let sort_regexp = new RegExp(String.raw`^\/\/ START <${record}\.(\S{4,}?)>`, 'gm');
-	
-	// insert it in alphabetical order
-	while(true) {
-	    let sort_match = sort_regexp.exec(cur_file_data);
-	    if (sort_match !== null) {
-		if (typeof sort_match[1] !== "undefined" && field.localeCompare(sort_match[1]) < 0) {
-		    let new_file_data = [
-			cur_file_data.slice(0, sort_match.index),
-			boilerplate_printer(name),
-			cur_file_data.slice(sort_match.index)
-		    ].join("");
-		    cur_file_data = new_file_data;
+
+    let printer_func = undefined;
+    // check if the we should map a generic printer to this record-field pair
+    if (common_data !== null && typeof common_data[field] !== "undefined") {
+	if (common_data[field].records.length === 0 || common_data[field].records.includes(record)) {
+	    printer_func = common_data[field].printer;
+	}
+    }
+
+    while (true) {
+	let last_match = cur_regexp.lastIndex;
+	let regexp_match = cur_regexp.exec(cur_file_data);
+	if (regexp_match !== null) {
+	    if (typeof regexp_match[1] !== "undefined") {
+		if (field === regexp_match[1]) {
+		    if (typeof printer_func === "undefined") break;
+
+		    if (typeof regexp_match[2] === "undefined" || regexp_match[2] !== printer_func) {
+			// do any updates here, commenting out the previous
+			let new_file_data = [
+			    cur_file_data.slice(0, regexp_match.index),
+			    // add out new common substiture
+			    boilerplate_printer(name, printer_func),
+			    cur_file_data.slice(regexp_match.index + regexp_match[0].length),
+			    // comment out previous code to not lose anything
+			    "\n\n" + boilerplate_removed(regexp_match[0])
+			].join("");
+			cur_file_data = new_file_data;
+		    }
 		    break;
 		}
-	    } else {
-		cur_file_data += boilerplate_printer(name);
-		break;
+		else if (field.localeCompare(regexp_match[1]) < 0) {
+		    let new_file_data = [
+			cur_file_data.slice(0, regexp_match.index),
+			boilerplate_printer(name, printer_func) + "\n\n",
+			cur_file_data.slice(regexp_match.index)
+		    ].join("");
+		    cur_file_data = new_file_data;
+		    cur_regexp.lastIndex = last_match;
+		    break;
+		}
 	    }
+	} else {
+	    if (last_match > 0) {
+		let new_file_data = [
+		    cur_file_data.slice(0, last_match),
+		    "\n\n" + boilerplate_printer(name, printer_func),
+		    cur_file_data.slice(last_match)
+		].join("");
+		cur_file_data = new_file_data;
+	    } else {
+		cur_file_data += "\n\n" + boilerplate_printer(name, printer_func);
+	    }
+	    break;
 	}
-    } else {
-	// do any updates here, commenting out the previous
-	//let new_file_data = [
-	//    cur_file_data.slice(0, regexp_match.index),
-	//    // comment out previous code to not lose anything
-	//    String.raw`/*${regexp_match[0]}*/`,
-	//    substitute_stuff_here,
-	//    cur_file_data.slice(regexp_match.index + regexp_match[0].length)
-	//].join("");
-	//cur_file_data = new_file_data;
     }
 }
 // to not miss last record type
